eef904b66cbaa2da74e2e9136c4a2d764667a6a5
kendsn123456 <138540084+kendsn123456@users.noreply.github.com>
2023-08-31 16:20:39 +0800

    Update ManualTransmission.cs

--- Assets/Scripts/ManualTransmission.cs
+++ Assets/Scripts/ManualTransmission.cs
@@ -2,28 +2,20 @@
 
 public class ManualTransmission : MonoBehaviour
 {
-    public int gear = 8; // 目前檔位，8 代表 R 檔
-    private float speed = 0.0f; // 目前車速
-    private bool isClutchPressed = false; // 是否按下離合器
+    public int gear = 8; // 初始檔位為 R 檔 (8)
+    private float speed = 0.0f;
+    private bool isClutchPressed = false;
 
     private void Update()
     {
-        // 根據玩家輸入切換檔位
-        //if (Input.GetKeyDown(KeyCode.Alpha0)) ChangeToNeutral();
-        //if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeGear(1);
-        //if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeGear(2);
-        //if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeGear(3);
-        //if (Input.GetKeyDown(KeyCode.Alpha8)) ChangeToReverse();
-        print(gear);
-
-        // 離合器控制邏輯
+        // 離合器控制
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            isClutchPressed = true;
+            isClutchPressed = true; // 當按下空白鍵時，表示離合器被按下
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            isClutchPressed = false;
+            isClutchPressed = false; // 當放開空白鍵時，表示離合器被釋放
         }
 
         // 檔位切換邏輯
@@ -33,49 +25,49 @@ public class ManualTransmission : MonoBehaviour
             {
                 if (gear == 0 || gear == 2)
                 {
-                    ChangeGear(1);
+                    ChangeGear(1); // 變換到一檔
                 }
             }
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
                 if (gear == 1 || gear == 3)
                 {
-                    ChangeGear(2);
+                    ChangeGear(2); // 變換到二檔
                 }
             }
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
                 if (gear == 2)
                 {
-                    ChangeGear(3);
+                    ChangeGear(3); // 變換到三檔
                 }
             }
             if (Input.GetKeyDown(KeyCode.Alpha8))
             {
                 if (gear == 0)
                 {
-                    ChangeToReverse();
+                    ChangeToReverse(); // 變換到 R 檔
                 }
             }
         }
 
-        // 控制速度邏輯
+        // 速度控制邏輯
         if (speed < GetMaxSpeedForGear(gear))
         {
-            speed += Time.deltaTime * 10.0f; // 調整速度增加率
+            speed += Time.deltaTime * 10.0f; // 根據檔位遞增速度
         }
     }
 
     private void ChangeToNeutral()
     {
-        gear = 0; // 切換到 N 檔
+        gear = 0; // 變換到 N 檔
     }
 
     private void ChangeToReverse()
     {
         if (gear == 0)
         {
-            gear = 8; // 切換到 R 檔
+            gear = 8; // 變換到 R 檔
         }
     }
 
@@ -83,13 +75,13 @@ public class ManualTransmission : MonoBehaviour
     {
         if (gear == 0 || (gear == 8 && newGear == 1))
         {
-            gear = newGear; // 允許切換檔位的情況下，切換檔位
+            gear = newGear; // 變換檔位
         }
     }
 
     private float GetMaxSpeedForGear(int gear)
     {
-        if (gear == 8) return GetMaxSpeedForGear(1) * 0.5f; // R 檔最大速度為一檔的一半
-        return gear * 10.0f; // 根據檔位設定速度值，可以進行調整
+        if (gear == 8) return GetMaxSpeedForGear(1) * 0.5f;
+        return gear * 10.0f; // 根據檔位計算最大速度
     }
 }
