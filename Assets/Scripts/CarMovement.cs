b3f5287807617b84e1a1f0bffd55fbfbd6c0a04c
FR3pL <ternplar@hotmail.com>
2023-09-25 16:15:37 +0800

    up

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 
-public class CarMovement : MonoBehaviour
+public class CarMovement : ManualTransmission
 {
-    public ManualTransmission transmissionScript; // 參考到 ManualTransmission 腳本
+    //public ManualTransmission transmissionScript; // 參考到 ManualTransmission 腳本
     public Transform frontWheelsR; // 右前輪的 Transform
     public Transform frontWheelsL; // 左前輪的 Transform
     public float maxSteeringAngle = 45.0f; // 最大轉彎角度
@@ -10,7 +10,7 @@ public class CarMovement : MonoBehaviour
 
     private Rigidbody carRigidbody; // 車輛的剛體
     private float currentSteeringAngle = 0.0f; // 目前轉彎角度
-    private bool isBraking = false; // 是否正在剎車
+    //private bool isBraking = false; // 是否正在剎車
 
     private void Start()
     {
@@ -20,7 +20,7 @@ public class CarMovement : MonoBehaviour
     private void Update()
     {
         // 轉向邏輯
-        if (transmissionScript.gear != 0) // 只有在不在 N 檔時才轉向
+        if (/*transmissionScript.*/gear != 0) // 只有在不在 N 檔時才轉向
         {
             float horizontalInput = Input.GetAxis("Horizontal"); // 玩家的水平輸入
             currentSteeringAngle = horizontalInput * maxSteeringAngle; // 根據輸入計算轉彎角度
@@ -30,38 +30,38 @@ public class CarMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // 加速和剎車邏輯
-        if (transmissionScript.gear != 0) // 只有在不在 N 檔時才移動
+        if (/*transmissionScript.*/gear != 0) // 只有在不在 N 檔時才移動
         {
             if (Input.GetKey(KeyCode.W)) // 按下前進鍵
             {
                 // 根據檔位調整加速力道
-                if (transmissionScript.gear == 1)
+                if (/*transmissionScript.*/gear == 1)
                 {
                     carRigidbody.AddForce(transform.forward * 1000.0f); // 調整加速力道
                 }
-                else if (transmissionScript.gear == 2)
+                else if (/*transmissionScript.*/gear == 2)
                 {
                     carRigidbody.AddForce(transform.forward * 1500.0f); // 調整加速力道
                 }
-                else if (transmissionScript.gear == 3)
+                else if (/*transmissionScript.*/gear == 3)
                 {
                     carRigidbody.AddForce(transform.forward * 2000.0f); // 調整加速力道
                 }
-                else if (transmissionScript.gear == 8) // R 檔
-                {
-                    carRigidbody.AddForce(-transform.forward * 500.0f); // 調整後退力道
-                }
+                //else if (/*transmissionScript.*/gear == 8) // R 檔
+                //{
+                //    carRigidbody.AddForce(-transform.forward * 500.0f); // 調整後退力道
+                //}
             }
 
             if (Input.GetKey(KeyCode.S)) // 按下剎車鍵
             {
-                isBraking = true;
+                //isBraking = true;
                 carRigidbody.AddForce(-transform.forward * brakeForce); // 剎車力道
             }
-            else
-            {
-                isBraking = false;
-            }
+            //else
+            //{
+            //    isBraking = false;
+            //}
         }
     }
 
--- Assets/Scripts/ManualTransmission.cs
+++ Assets/Scripts/ManualTransmission.cs
@@ -2,8 +2,8 @@
 
 public class ManualTransmission : MonoBehaviour
 {
-    public int gear = 8; // 初始檔位為 R 檔 (8)
-    private float speed = 0.0f;
+    public int gear = 0; // 初始檔位為 N 檔 (0)
+    public float speed = 0.0f;
     private bool isClutchPressed = false;
 
     private void Update()
@@ -17,7 +17,7 @@ public class ManualTransmission : MonoBehaviour
         {
             isClutchPressed = false; // 當放開空白鍵時，表示離合器被釋放
         }
-
+        print(gear);
         // 檔位切換邏輯
         if (isClutchPressed)
         {
@@ -25,30 +25,30 @@ public class ManualTransmission : MonoBehaviour
             {
                 if (gear == 0 || gear == 2)
                 {
-                    ChangeGear(1); // 變換到一檔
+                    /*ChangeGear(1)*/gear=1; // 變換到一檔
                 }
             }
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
                 if (gear == 1 || gear == 3)
                 {
-                    ChangeGear(2); // 變換到二檔
+                    /*ChangeGear(2)*/gear=2; // 變換到二檔
                 }
             }
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
                 if (gear == 2)
                 {
-                    ChangeGear(3); // 變換到三檔
+                    /*ChangeGear(3)*/gear=3; // 變換到三檔
                 }
             }
-            if (Input.GetKeyDown(KeyCode.Alpha8))
-            {
-                if (gear == 0)
-                {
-                    ChangeToReverse(); // 變換到 R 檔
-                }
-            }
+            //if (Input.GetKeyDown(KeyCode.Alpha8))
+            //{
+            //    if (gear == 0)
+            //    {
+            //        /*ChangeToReverse()*/gear=8; // 變換到 R 檔
+            //    }
+            //}
         }
 
         // 速度控制邏輯
@@ -66,25 +66,25 @@ public class ManualTransmission : MonoBehaviour
         }
     }    
 
-    private void ChangeToReverse()
-    {
-        if (gear == 0)
-        {
-            gear = 8; // 變換到 R 檔
-        }
-    }
+    //private void ChangeToReverse()
+    //{
+    //    if (gear == 0)
+    //    {
+    //        gear = 8; // 變換到 R 檔
+    //    }
+    //}
 
-    private void ChangeGear(int newGear)
-    {
-        if (gear == 0 || (gear == 8 && newGear == 1))
-        {
-            gear = newGear; // 變換檔位
-        }
-    }
+    //private void ChangeGear(int newGear)
+    //{
+    //    if (gear == 0 || (gear == 8 && newGear == 1))
+    //    {
+    //        gear = newGear; // 變換檔位
+    //    }
+    //}
 
     private float GetMaxSpeedForGear(int gear)
     {
-        if (gear == 8) return GetMaxSpeedForGear(1) * 0.5f;
+        //if (gear == 8) return GetMaxSpeedForGear(1) * 0.5f;
         return gear * 10.0f; // 根據檔位計算最大速度
     }
 }
