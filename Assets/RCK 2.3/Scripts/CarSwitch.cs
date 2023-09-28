055ea5510d250dc5e8848eba2a0d393ee29cbeac
FR3pL <ternplar@hotmail.com>
2023-09-28 16:28:16 +0800

    up

--- Assets/RCK 2.3/Scripts/CarSwitch.cs
+++ Assets/RCK 2.3/Scripts/CarSwitch.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+//using System.Collections;
 
 public class CarSwitch : MonoBehaviour
 {
@@ -7,19 +7,15 @@ public class CarSwitch : MonoBehaviour
     public Transform[] Cars;
     public Transform MyCamera;
 
-
     public void CurrentCarActive(int current)
     {
-
         int amount = 0;
-
         foreach (Transform Car in Cars)
         {
             if (current == amount)
             {
                 MyCamera.GetComponent<VehicleCamera>().target = Car;
-
-                MyCamera.GetComponent<VehicleCamera>().Switch = 0;
+                //MyCamera.GetComponent<VehicleCamera>().Switch = 0;
                 MyCamera.GetComponent<VehicleCamera>().cameraSwitchView = Car.GetComponent<VehicleControl>().carSetting.cameraSwitchView;
                 Car.GetComponent<VehicleControl>().activeControl = true;
             }
@@ -27,12 +23,7 @@ public class CarSwitch : MonoBehaviour
             {
                 Car.GetComponent<VehicleControl>().activeControl = false;
             }
-
             amount++;
         }
     }
-
-
-
-
 }
